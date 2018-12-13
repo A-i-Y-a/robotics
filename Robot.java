11841c6316a91ac2415c0e0dfd11d95ccb80e334
Matt Hui <mhui@ucvts.tec.nj.us>
2018-12-13 20:38:17 +0000

    wo bu hui xie Java

---
+++ Robot.java
@@ -0,0 +1,64 @@
+import edu.wpi.first.wpilibj.*;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+class Robot extends TimedRobot {
+
+    WPI_TalonSRX motorFL;
+    WPI_TalonSRX motorFR;
+    WPI_TalonSRX motorBL;
+    WPI_TalonSRX motorBR;
+    SpeedControllerGroup right;
+    SpeedControllerGroup left;
+    DifferentialDrive drive;
+    XboxController controller;
+    
+    void robotInit() {
+        motorFL = new WPI_TalonSRX(1);
+        motorFR = new WPI_TalonSRX(2);
+        motorBL = new WPI_TalonSRX(3);
+        motorBR = new WPI_TalonSRX(4);
+        
+        controller = new XboxController(0);
+        
+        left = new SpeedControllerGroup(motorFL, motorBL);
+        right = new SpeedControllerGroup(motorFR, motorBR);
+        
+        drive = new DifferentDrive(left, right);
+    }
+
+    void teleopInit() {
+        
+    }
+    
+    void teleopPeriodic() {
+        
+
+        if (controller.getAButton() == true) {
+            
+            double driveSpeedLeft = controller.getY(GenericHID.Hand.kLeft);
+            double turnSpeedLeft = controller.getX(GenericHID.Hand.kLeft);
+            drive.arcadeDrive(driveSpeedLeft, turnSpeedLeft);
+        }
+        
+        else if (controller.getBumper (GenericHID.hand left) == true) {
+            
+            double driveSpeedLeft = controller.getY(GenericHID.Hand.kLeft);
+            double turnSpeedRight = controller.getX(GenericHID.Hand.kRight);
+            drive.arcadeDrive(driveSpeedLeft, turnSpeedRight);
+        }
+        
+        else if (controller.getBumper (GenericHID.hand right) == true) {
+            
+            double driveSpeedRight = controller.getY(GenericHID.Hand.kRight);
+            double turnSpeedLeft = controller.getX(GenericHID.Hand.kLeft);
+            drive.arcadeDrive(driveSpeedRight, turnSpeedLeft);
+        }
+        
+        //spinning
+        else if (controller.getAButton() == true) {
+        
+            drive.arcadeDrive(0, -1);
+        }
+    }
+    
+}
